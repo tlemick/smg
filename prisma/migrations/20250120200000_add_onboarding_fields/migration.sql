268b1676309069aa709bbe1c2c4733efc388bf1c
Travis <tlemick@gmail.com>
2026-01-23 10:50:33 -0800

    migration: Add onboarding fields to User model

---
+++ prisma/migrations/20250120200000_add_onboarding_fields/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN IF NOT EXISTS "hasCompletedOnboarding" BOOLEAN NOT NULL DEFAULT false;
+ALTER TABLE "User" ADD COLUMN IF NOT EXISTS "onboardingCompletedAt" TIMESTAMP(3);
